d663e61d4c741e3d3477c51325c6b37420965985
Francisco Cortes <cortes.frj@gmail.com>
2023-01-14 19:27:28 -0500

    adding whitenoinse

--- stories/settings.py
+++ stories/settings.py
@@ -39,17 +39,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
+    'whitenoise.runserver_nostatic',
+
     'blog.apps.BlogConfig',
     'desktop.apps.DesktopConfig',
     'frontend.apps.FrontendConfig',
-    'accounts.apps.AccountsConfig', 
+    'accounts.apps.AccountsConfig',
 
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,7 +138,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
