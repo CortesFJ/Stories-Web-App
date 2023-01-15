d634142c9db94ebc5faaf46ed0a46390adfa9846
Francisco Cortes <cortes.frj@gmail.com>
2023-01-14 20:31:17 -0500

    setting new url in ALLOWED_HOSTS

--- stories/settings.py
+++ stories/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2bij%!@wjcdifjoxjnf=j%642x26h-%%@a26r2nzoc7hbjnk+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 1
+DEBUG = 0
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['http://stories']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://stories-web-app-production.up.railway.app/']
 
 
 # Application definition
