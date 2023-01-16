28059b99131267ae54dc18a958bd1cc48ded1ae0
Francisco Cortes <cortes.frj@gmail.com>
2023-01-16 14:43:43 -0500

    settinf DEBUG to False

--- stories/settings.py
+++ stories/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2bij%!@wjcdifjoxjnf=j%642x26h-%%@a26r2nzoc7hbjnk+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 1
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
