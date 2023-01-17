b720b43ac28f5bdc52a984af4bd4d60705a20cba
Francisco Cortes <cortes.frj@gmail.com>
2023-01-16 21:36:35 -0500

    remmove uninteresting code

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.sendArticles, name='blog'),
-    # path("hv/", views.hv),
-]
--- blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def sendArticles(request):
-    context = {}
-    return render(request, "blog/home.html", context=context)
-
-# def hv(request):
-#     return render(request, "blog/hv.html")
