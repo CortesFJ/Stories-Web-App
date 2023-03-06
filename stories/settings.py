08291844930134c5f40b6968542d9266ea4fa5a1
Francisco Cortes <cortes.frj@gmail.com>
2023-03-05 20:52:18 -0500

    update

--- stories/settings.py
+++ stories/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_URL = 'assets/'
+STATIC_URL = 'static/'
+# STATIC_URL = 'assets/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
