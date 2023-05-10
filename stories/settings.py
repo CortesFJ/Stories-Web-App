e322ab0d73c6790f1cc4a7c2a1c256382249923c
Francisco Cortes <cortes.frj@gmail.com>
2023-05-10 00:21:04 -0500

    trying to deploy

--- stories/settings.py
+++ stories/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2bij%!@wjcdifjoxjnf=j%642x26h-%%@a26r2nzoc7hbjnk+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
