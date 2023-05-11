c69f89a4adf5fa16c49c2688d18595e79280e5c8
Francisco Cortes <cortes.frj@gmail.com>
2023-05-11 01:44:56 -0500

    setting to deploy

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.Home, name='blog'),
-    path("hv/", views.Hv, name='blog'),
+    # path("hv/", views.Hv, name='blog'),
     path("about/", views.About, name='about'),
     path("tools/", views.Tools, name='tools-blog'),
 ]
